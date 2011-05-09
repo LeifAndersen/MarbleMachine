ad0460b358e5b6a8d9a3ab91e46a01378c9c9565
Leif Andersen <leif@leifandersen.net>
2011-05-08 20:39:59 -0600

    Added two more APIs for the native machine

--- android/jni/gl_code.cpp
+++ android/jni/gl_code.cpp
@@ -1,8 +1,12 @@
 #include <jni.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
 
 extern "C" {
     JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_init(JNIEnv * env, jobject obj, jint width, jint height);
     JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_draw(JNIEnv * env, jobject obj);
+    JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_touch(JNIEnv * env, jobject obj, jint finger, jint x, jint y);
+    JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_release(JNIEnv * env, jobject obj, jint finger);
 };
 
 JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_init(JNIEnv * env, jobject obj, jint width, jint height)
@@ -12,6 +16,16 @@ JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLi
 
 
 JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_draw(JNIEnv * env, jobject obj)
+{
+
+}
+
+JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_touch(JNIEnv * env, jobject obj, jint finger, jint x, jint y)
+{
+
+}
+
+JNIEXPORT void JNICALL Java_net_leifandersen_mobile_android_marblemachine_MainLib_release(JNIEnv * env, jobject obj, jint finger)
 {
 
 }
--- android/src/net/leifandersen/mobile/android/marblemachine/MainLib.java
+++ android/src/net/leifandersen/mobile/android/marblemachine/MainLib.java
@@ -7,4 +7,6 @@ public class MainLib {
     
     public static native void init(int width, int height);
     public static native void draw();
+    public static native void touch(int finger, int x, int y);
+    public static native void release(int finger);
 }
