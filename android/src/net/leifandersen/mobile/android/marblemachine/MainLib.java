29c20682d1d5c1a98ff2609c4e0756ec4bbb1bb7
Leif Andersen <leif@leifandersen.net>
2011-05-10 18:16:36 -0600

    Updated the gl_code to use the core api calls.

--- android/src/net/leifandersen/mobile/android/marblemachine/MainLib.java
+++ android/src/net/leifandersen/mobile/android/marblemachine/MainLib.java
@@ -7,6 +7,12 @@ public class MainLib {
     
     public static native void init(int width, int height);
     public static native void draw();
+
     public static native void touch(int finger, int x, int y);
+    public static native void move(int finger, int x, int y);
     public static native void release(int finger);
+
+    public static native void pauseGame();
+    public static native void resumeGame();
+    public static native void resetGame();
 }
