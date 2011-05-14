d89ebca915e2a32d7c545a9a26a717264f157e04
Leif Andersen <leif@leifandersen.net>
2011-05-13 19:13:14 -0600

    COmmets

--- core/point.h
+++ core/point.h
@@ -7,12 +7,36 @@ public:
     Point();
     Point(float x, float y, float z);
 
+    /**
+      * Vector Addition
+      */
     Point operator +(const Point & other);
+
+    /**
+      * Vector Subtraction
+      */
     Point operator -(const Point & other);
+
+    /**
+      * Scaler Multiplication
+      */
     Point operator *(const float scale);
+
+    /**
+      * Vector Adition
+      */
     Point & operator +=(const Point & other);
+
+    /**
+      * Vector Subraction
+      */
     Point & operator -=(const Point & other);
+
+    /**
+      * Scaler Multiplication
+      */
     Point & operator *=(const float scale);
+
     float x;
     float y;
     float z;
