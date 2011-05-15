d72c2138a965d1890b548dde63e2d5b6f0753654
Leif Andersen <leif@leifandersen.net>
2011-05-14 21:48:37 -0600

    Made cannon class

---
+++ core/entities/cannon.cpp
@@ -0,0 +1,5 @@
+#include "cannon.h"
+
+Cannon::Cannon()
+{
+}
---
+++ core/entities/cannon.h
@@ -0,0 +1,12 @@
+#ifndef CANNON_H
+#define CANNON_H
+
+#include "entity.h"
+
+class Cannon : public Entity
+{
+public:
+    Cannon();
+};
+
+#endif // CANNON_H
