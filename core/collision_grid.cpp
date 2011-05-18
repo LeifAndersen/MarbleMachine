f9ad7ea70d02ddca8a3b66c1fcd2d6d33c3a157a
Daniel Blakemore <DanBlakemore@gmail.com>
2011-05-17 19:47:49 -0600

    Completely implemented and untested CollisionGrid class.

--- core/collision_grid.cpp
+++ core/collision_grid.cpp
@@ -1,5 +1,233 @@
 #include "collision_grid.h"
+#include "math.h"
+#include "stdlib.h"
 
-collision_grid::collision_grid()
-{
+bool CollisionGrid::collide(int x, int y, int r, Point obj) {
+    int wid = (x * partitionSize) / 2;
+    int hei = (y * partitionSize) / 2;
+
+    int distx = abs(obj.x - (x * partitionSize));
+    int disty = abs(obj.y - (y * partitionSize));
+
+    if (distx > (wid + r)) { return false; }
+    if (disty > (hei + r)) { return false; }
+
+    if (distx <= (wid)) { return true; }
+    if (disty <= (hei)) { return true; }
+
+    int cornerDistance_sq = (distx - wid) * (distx - wid) +
+        (disty - hei) * (disty - hei);
+
+    return (cornerDistance_sq <= (r * r));
+}
+
+/**
+  * Constructor for this data structure
+  *
+  * Input:
+  *     partitionSize: is the width of the squares of the grid.
+  *     xSize: the max x dimmension from (0, 0)
+  *     ySize: the max y dimmension from (0, 0)
+  */
+CollisionGrid::CollisionGrid(int _partitionSize, int xSize, int ySize) {
+    int xNodes = ceil(xSize / _partitionSize);
+    int yNodes = ceil(ySize / _partitionSize);
+
+    partitionSize = _partitionSize;
+
+    grid.reserve(xNodes);
+
+    for (int i = 0; i < xNodes; i++) {
+        grid[i].reserve(yNodes);
+    }
+}
+
+/**
+  * Add a plank to the grid
+  *
+  * Input:
+  *     plank: pointer to the plank to add.  YAY.
+  */
+void CollisionGrid::addPlank(Plank * plank) {
+    int psuedoradius = sqrt(pow(plank->position.x, 2) + pow(plank->position.y, 2));
+    int x = 0;
+    int y = 0;
+
+    for (std::vector<std::vector<GridNode> >::iterator i = grid.begin(); i != grid.end(); i++, x++) {
+        for(std::vector<GridNode>::iterator j = i->begin(); j != i->end(); j++, y++) {
+            if (collide(x, y, psuedoradius, plank->position)) {
+                j->planks.push_back(plank);
+            }
+        }
+    }
+}
+
+/**
+  * Add a cannon to the grid
+  *
+  * Input:
+  *     plank: pointer to the cannon to add.
+  */
+void CollisionGrid::addCannon(Cannon * cannon) {
+    int psuedoradius = sqrt(pow(cannon->position.x, 2) + pow(cannon->position.y, 2));
+    int x = 0;
+    int y = 0;
+
+    for (std::vector<std::vector<GridNode> >::iterator i = grid.begin(); i != grid.end(); i++, x++) {
+        for(std::vector<GridNode>::iterator j = i->begin(); j != i->end(); j++, y++) {
+            if (collide(x, y, psuedoradius, cannon->position)) {
+                j->cannons.push_back(cannon);
+            }
+        }
+    }
+}
+
+/**
+  * Remove a plank from the grid
+  *
+  * Input:
+  *     plank: pointer to the plank to remove.
+  */
+void CollisionGrid::removePlank(Plank * plank) {
+    std::list<Plank *>::iterator toRemove;
+    bool remove = false;
+
+    for (std::vector<std::vector<GridNode> >::iterator i = grid.begin(); i != grid.end(); i++) {
+        for(std::vector<GridNode>::iterator j = i->begin(); j != i->end(); j++) {
+            for (std::list<Plank *>::iterator k = j->planks.begin(); k != j->planks.end(); k++) {
+                if ((*k) == plank) {
+                    toRemove = k;
+                    break;
+                }
+            }
+            if (remove) {
+                j->planks.erase(toRemove);
+                remove = false;
+            }
+        }
+    }
+}
+
+/**
+  * Remove a cannon from the grid
+  *
+  * Input:
+  *     plank: pointer to the cannon to remove.
+  */
+void CollisionGrid::removeCannon(Cannon * cannon) {
+    std::list<Cannon *>::iterator toRemove;
+    bool remove = false;
+
+    for (std::vector<std::vector<GridNode> >::iterator i = grid.begin(); i != grid.end(); i++) {
+        for(std::vector<GridNode>::iterator j = i->begin(); j != i->end(); j++) {
+            for (std::list<Cannon *>::iterator k = j->cannons.begin(); k != j->cannons.end(); k++) {
+                if ((*k) == cannon) {
+                    toRemove = k;
+                    break;
+                }
+            }
+            if (remove) {
+                j->cannons.erase(toRemove);
+                remove = false;
+            }
+        }
+    }
+}
+
+/**
+  * Get all the planks near the coordinates
+  *
+  * Input:
+  *     x: x coordinate
+  *     y: y coordinate
+  */
+std::vector<Plank *> CollisionGrid::getPlanks(float x, float y) {
+    std::list<Plank *> planks;
+
+    int gridX = (int)floor(x) - ((int)floor(x) % partitionSize);
+    int gridY = (int)floor(y) - ((int)floor(y) % partitionSize);
+
+    int startX = 0;
+    int endX = 0;
+    int startY = 0;
+    int endY = 0;
+
+    if(gridX == 0) {
+        startX = 0;
+    } else {
+        startX = gridX - 1;
+    }
+
+    if(gridY == 0) {
+        startY = 0;
+    } else {
+        startY = gridY - 1;
+    }
+
+    if((gridX + 1) % grid.size() == 0) {
+        endX = gridX;
+    } else {
+        endX = (gridX + 1) % grid.size();
+    }
+
+    if((gridY + 1) % grid[0].size() == 0) {
+        endY = gridY;
+    } else {
+        endY = (gridY + 1) % grid[0].size();
+    }
+
+    for(int i = startX; i <= endX; i++) {
+        for(int j = startY; j <= endY; j++) {
+            planks.splice(grid[i][j].planks.begin(), grid[i][j].planks);
+        }
+    }
+}
+
+/**
+  * Get all the cannons near the coordinates
+  *
+  * Input:
+  *     x: x coordinate
+  *     y: y coordinate
+  */
+std::vector<Plank *> CollisionGrid::getCannons(float x, float y) {
+    std::list<Cannon *> cannons;
+
+    int gridX = (int)floor(x) - ((int)floor(x) % partitionSize);
+    int gridY = (int)floor(y) - ((int)floor(y) % partitionSize);
+
+    int startX = 0;
+    int endX = 0;
+    int startY = 0;
+    int endY = 0;
+
+    if(gridX == 0) {
+        startX = 0;
+    } else {
+        startX = gridX - 1;
+    }
+
+    if(gridY == 0) {
+        startY = 0;
+    } else {
+        startY = gridY - 1;
+    }
+
+    if((gridX + 1) % grid.size() == 0) {
+        endX = gridX;
+    } else {
+        endX = (gridX + 1) % grid.size();
+    }
+
+    if((gridY + 1) % grid[0].size() == 0) {
+        endY = gridY;
+    } else {
+        endY = (gridY + 1) % grid[0].size();
+    }
+
+    for(int i = startX; i <= endX; i++) {
+        for(int j = startY; j <= endY; j++) {
+            cannons.splice(grid[i][j].cannons.begin(), grid[i][j].cannons);
+        }
+    }
 }
--- core/collision_grid.h
+++ core/collision_grid.h
@@ -3,6 +3,9 @@
 
 #include "plank.h"
 #include "cannon.h"
+#include "point.h"
+#include "list"
+#include "vector"
 
 class CollisionGrid
 {
@@ -15,7 +18,7 @@ public:
       *     xSize: the max x dimmension from (0, 0)
       *     ySize: the max y dimmension from (0, 0)
       */
-    collision_grid(int partitionSize, int xSize, int ySize);
+    CollisionGrid(int _partitionSize, int xSize, int ySize);
 
     /**
       * Adda a plank to the grid
@@ -23,7 +26,7 @@ public:
       * Input:
       *     plank: reference to the plank to add.  YAY.
       */
-    void addPlank(Plank & plank);
+    void addPlank(Plank * plank);
 
     /**
       * Add a cannon to the grid
@@ -31,7 +34,7 @@ public:
       * Input:
       *     plank: reference to the cannon to add.
       */
-    void addCannon(Cannon & cannon);
+    void addCannon(Cannon * cannon);
 
     /**
       * Remove a plank from the grid
@@ -39,7 +42,7 @@ public:
       * Input:
       *     plank: reference to the plank to remove.
       */
-    void removePlank(Plank & plank);
+    void removePlank(Plank * plank);
 
     /**
       * Remove a cannon from the grid
@@ -47,7 +50,7 @@ public:
       * Input:
       *     plank: reference to the cannon to remove.
       */
-    void removeCannon(Cannon & cannon);
+    void removeCannon(Cannon * cannon);
 
     /**
       * Get all the planks near the coordinates
@@ -56,7 +59,7 @@ public:
       *     x: x coordinate
       *     y: y coordinate
       */
-    std::vector<Plank &> getPlanks(float x, float y);
+    std::vector<Plank *> getPlanks(float x, float y);
 
     /**
       * Get all the cannons near the coordinates
@@ -65,8 +68,28 @@ public:
       *     x: x coordinate
       *     y: y coordinate
       */
-    std::vector<Plank &> getCannons(float x, float y);
+    std::vector<Plank *> getCannons(float x, float y);
 
+private:
+
+    struct GridNode {
+        std::list<Cannon *> cannons;
+        std::list<Plank *> planks;
+    };
+
+    /**
+      * Do circle sqaure collision for the grid square at x and y
+      *
+      * Input:
+      *     x: x coordinate of grid square
+      *     y: y coordinate of grid square
+      *     r: radius of circle
+      *     obj: the position of the circle
+      */
+    bool collide(int x, int y, int r, Point obj);
+
+    int partitionSize;
+    std::vector<std::vector<GridNode> > grid;
 };
 
 #endif // COLLISIONGRID_H
