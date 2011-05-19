d899067060d42844d32e41dde6c9736727a5f7d2
Leif Andersen <leif@leifandersen.net>
2011-05-19 15:20:21 -0600

    Moved normal to rotatable entity.

--- core/entities/rotatable_entity.h
+++ core/entities/rotatable_entity.h
@@ -10,6 +10,8 @@ public:
     static void loadData();
     virtual void draw();
     float rotation;
+    Point normal; // TODO -- make sure the normal is always the side that is facing rotation + 90º
+                  // based on the assumption that a plank lying flat has a rotation of 0º
 private:
     enum State { PLACE, MOVE, ROTATE };
     State state;
