d25341c8c64c279d1110699b0cc4c5ee6667fb20
Leif Andersen <leif@leifandersen.net>
2011-05-24 19:00:46 -0600

    Program doesn't crash now

--- core/game_state.cpp
+++ core/game_state.cpp
@@ -4,7 +4,7 @@ GameState::GameState() : level(0), grid(0, 0, 0), stopLooping(true),
     marbleInCannon(false), timeInCannon(0), engine(*this),
     menu(*this), importer(*this)
 {
-    importer.loadDrawables();
+    //importer.loadDrawables();
 }
 
 GameState::~GameState()
