1940a8aece492018877596293387f3690fafdc2f
Leif Andersen <leif@leifandersen.net>
2011-05-26 17:27:42 -0600

    os_calls.mm updated, I hope

--- ios/MarbleMachine/os_calls.mm
+++ ios/MarbleMachine/os_calls.mm
@@ -8,6 +8,8 @@
 
 #include "os_calls.h"
 #include <AudioToolbox/AudioToolbox.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 // TODO: Maybe there's a problem casting between the sound id int and the uint32 SystemSoundID used by the iOS frameworks
 void playSound(int soundID)
@@ -38,4 +40,62 @@ int loadSound(std::string sound)
 void unloadSound(int soundID)
 {
     AudioServicesDisposeSystemSoundID((SystemSoundID)soundID);
+}
+
+// File handles
+// Directly maps to fopen/fclose/fchdir/etc.  Look at the man pages for docs.
+typedef void MMFILE;
+MMFILE * MMfopen(const char * path)
+{
+    // TODO: find the actual path needed
+    return (MMFILE *) fopen(path);
+}
+
+void MMfclose(MMFILE * file)
+{
+    fclose((FILE*)file);
+}
+
+void MMfchdir(const char * dir)
+{
+    chdir(dir);
+}
+
+int MMfgetc(MMFILE * stream)
+{
+    return fgetc((FILE*)stream);
+}
+
+char * MMfgets(char * s, int size, MMFILE * stream)
+{
+    return fgets(s, size, (FILE*)stream);
+}
+
+long MMfseek(MMFILE * stream, long offset, int type)
+{
+    return fseek((FILE*)stream, offset, type);
+}
+
+long MMftell(MMFILE * stream)
+{
+    return ftell((FILE*)stream);
+}
+
+size_t MMfread(void * ptr, size_t size, size_t nmemb, MMFILE * stream)
+{
+    return fread(ptr, size, nmemb, (FILE*)stream);
+}
+
+int MMfeof(MMFILE * stream)
+{
+    return feof((FILE*)stream);
+}
+
+/**
+  * Get the size of the file in bytes
+  */
+size_t MMfsize(MMFILE * stream) 
+{
+    // TODO: implement if this ever gets used.
+    return NULL;
 }
