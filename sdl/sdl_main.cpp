301f9169727d89808abd85532cc01c0f0ac989dd
Leif Andersen <leif@leifandersen.net>
2011-06-26 22:39:37 -0600

    Well, it sort of works, if you odn't mind a crashed screen.

--- sdl/sdl_main.cpp
+++ sdl/sdl_main.cpp
@@ -1,4 +1,62 @@
+#include <SDL/SDL.h>
+
+#include "main.h"
+
+// TODO: Make dynamic
+#define WIDTH 640
+#define HEIGHT 480
+
+using namespace std;
+
+static bool quit = false;
+
 int main()
 {
+    // Set up SDL for opengl
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE) < 0) {
+        fprintf(stderr, "Video initialization failed: %s\n", SDL_GetError());
+        return 1;
+    }
+    const SDL_VideoInfo* info = SDL_GetVideoInfo();
+    if (!info) {
+        fprintf(stderr, "Video query failed: %s\n", SDL_GetError());
+        return 1;
+    }
+    int w = WIDTH > 0 ? WIDTH : info->current_w;
+    int h = HEIGHT > 0 ? HEIGHT : info->current_h;
+    int bpp = info->vfmt->BitsPerPixel;
 
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    int flags = SDL_OPENGL | SDL_FULLSCREEN | SDL_HWSURFACE | SDL_DOUBLEBUF;
+
+    SDL_Surface * screen = SDL_SetVideoMode(w, h, bpp, flags);
+    if (screen == 0) {
+        fprintf(stderr, "Video mode set failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Set up opengl
+    initGL();
+    updateGL(w, h);
+
+    // A few more things
+    SDL_WM_GrabInput(SDL_GRAB_ON);
+    SDL_ShowCursor(SDL_DISABLE);
+
+    // Set up and and start game running
+    setupGame();
+    startGame();
+    while(1) {
+        if(quit) {
+            pauseGame();
+            stopGame();
+            break;
+        }
+    }
+    return 0;
 }
