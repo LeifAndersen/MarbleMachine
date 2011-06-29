2207cad727a73b6362f7d94ecebb2f11ce015d49
Daniel Blakemore <DanBlakemore@gmail.com>
2011-06-29 13:51:04 -0600

    Some stuff to draw a grid to the level view.
    This also has the beginnings of implementing 
    QGraphicsView/Scene.

--- tools/GravityWell/GravityWellLevelEditor/level.h
+++ tools/GravityWell/GravityWellLevelEditor/level.h
@@ -1,18 +1,28 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
-#include <QWidget>
+#include <QGraphicsView>
 
-class Level : public QWidget
+class Level : public QGraphicsView
 {
     Q_OBJECT
 public:
     explicit Level(QWidget *parent = 0);
 
+    void showGrid();
+
 signals:
 
 public slots:
 
+private:
+    void makeGrid(int spacing);
+
+    QVector<QPoint> gridLines;
+    int gridWidth;
+    int gridHeight;
+    int gridSpacing;
+    bool drawGrid;
 };
 
 #endif // LEVEL_H
