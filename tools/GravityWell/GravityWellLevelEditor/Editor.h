c487aa1d3a9d7c8a75dcc985344e8d081fc34bb7
Daniel Blakemore <DanBlakemore@gmail.com>
2011-07-12 15:36:35 -0600

    Filling in signals and slots for basic UI.
    For my next trick, I will be makeing things 
    respond to clicks (I hope).

--- tools/GravityWell/GravityWellLevelEditor/Editor.h
+++ tools/GravityWell/GravityWellLevelEditor/Editor.h
@@ -14,6 +14,26 @@ public:
     ~Editor();
 
     Level * level;
+
+signals:
+    void changeItemTo(int);
+
+public slots:
+    void add();
+    void changeItem(int);
+    void setXPos(QString);
+    void setYPos(QString);
+    void setXVel(QString);
+    void setYVel(QString);
+    void setMass(QString);
+private:
+    // fields for holding object data
+    double xVel;
+    double yVel;
+    double xPos;
+    double yPos;
+    double mass;
+    Level::levelItem currentItem;
 };
 
 #endif // EDITOR_H
