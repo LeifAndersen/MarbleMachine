62b600b0f01e32cb2e633a418bbc9605d943944f
Leif Andersen <leif@leifandersen.net>
2011-07-18 20:54:24 -0600

    More menu button stuff.

--- core/input_converter.cpp
+++ core/input_converter.cpp
@@ -6,14 +6,35 @@ InputConverter::InputConverter(GameState & state) : state(state)
 
 void InputConverter::move(int finger, float x, float y)
 {
+    // Preliminary checks
     if(finger >= MAX_FINGERS)
         return;
+    fingerCoords[finger].x = x;
+    fingerCoords[finger].y = y;
+
+    // Menu button
+    switch(state.menuButton.state) {
+    case BUTTON_STATE_UP:
+        break;
+    case BUTTON_STATE_DOWN:
+        break;
+    case BUTTON_STATE_HOVER:
+        break;
+    }
 }
 
 void InputConverter::touch(int finger, float x, float y)
 {
+    // Preliminary checks
     if(finger >= MAX_FINGERS)
         return;
+    fingerCoords[finger].x = x;
+    fingerCoords[finger].y = y;
+
+    // Menu button
+    if(fingerOnButton(state.menuButton, fingerCoords[finger])) {
+        state.menuButton.state = BUTTON_STATE_DOWN;
+    }
 }
 
 void InputConverter::release(int finger, bool canceled)
