2c5daaa6a2fbe8464db95f3641a6dececdbf1d6f
Leif Andersen <leif@leifandersen.net>
2011-07-28 18:15:52 -0600

    Updated sector_exporter.

--- tools/sector_exporter.py
+++ tools/sector_exporter.py
@@ -1,6 +1,9 @@
 import struct
 import sys
 
+TEX_WIDTH  = 2048
+TEX_HEIGHT = 2048
+
 class Level:
     x = 1.0
     y = 1.0
@@ -16,20 +19,26 @@ def save(infile_name, outfile_name):
     x2 = 1.0
     y1 = 1.0
     y2 = 1.0
+    width = 1
+    height = 1
 
     # Read data
     for line in fin:
         (command, trash, data) = line.partition(':')
         if command == 'name':
             name = data
+        elif command == 'width':
+            width = float(data)
+        elif command == 'height':
+            height = float(data)
         elif command == 'coords':
             (topLeft, trash, bottomRight) = data.partition(';')
             (x,trash,y) = topLeft.partition(',')
-            x0 = float(x) *  2 / width  - 1
-            y0 = float(y) * -2 / height + 1
+            x0 = float(x) / TEX_WIDTH
+            y0 = float(y) / TEX_HEIGHT
             (x,trash,y) = bottomRight.partition(',')
-            x1 = float(x) *  2 / width  - 1
-            y1 = float(y) * -2 / height + 1
+            x1 = float(x) / TEX_WIDTH
+            y1 = float(y) / TEX_HEIGHT
         elif command == 'level':
             level = Level()
             (coords, trash, rad_str) = data.partition(';')
