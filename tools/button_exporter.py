eacdb3e43fc52493abbe997f33ebd9d75427c897
Leif Andersen <leif@leifandersen.net>
2011-08-03 15:48:28 -0600

    Changed binary converter to match new output to be merged in.

--- tools/button_exporter.py
+++ tools/button_exporter.py
@@ -5,41 +5,59 @@ import struct
 WIDTH = 2048
 HEIGHT = 2048
 
-def write_coords(coords, fout):
-        (top_left, trash, bottom_right) = coords.partition(';')
-        (top_left_x, trash, top_left_y) = top_left.partition(',')
-        (bottom_right_x, trash, bottom_right_y) = bottom_right.partition(',')
-        x0 = float(top_left_x)     *  2 / WIDTH   - 1
-        x1 = float(bottom_right_x) *  2 / WIDTH   - 1
-        y0 = float(top_left_y)     * -2 / HEIGHT  + 1
-        y1 = float(bottom_right_y) * -2 / HEIGHT  + 1
+class State:
+    x0 = 0
+    x1 = 0
+    y0 = 0
+    y1 = 0
 
-        # Write the data
-        fout.write(struct.pack('ffffffff', -1,  1, 0, 0, 0, 1, x0, y0))
-        fout.write(struct.pack('ffffffff',  1,  1, 0, 0, 0, 1, x1, y0))
-        fout.write(struct.pack('ffffffff',  1, -1, 0, 0, 0, 1, x1, y1))
-        fout.write(struct.pack('ffffffff', -1, -1, 0, 0, 0, 1, x0, y1))
+def parse_state(line, state):
+    (top_left, trash, bottom_right) = line.partition(';')
+    (x0, trash, y0) = top_left.partition(',')
+    (x1, trash, y1) = bottom_right.partition(',')
+    state.x0 =  float(x0) / WIDTH
+    state.x1 =  float(x1) / WIDTH
+    state.y0 = -float(y0) / HEIGHT
+    state.y1 = -float(y1) / HEIGHT
 
+def write_state(fout, state):
+    fout.write(struct.pack('ffffffff', -1,  1, 0, 0, 0, 1, state.x0, state.y0))
+    fout.write(struct.pack('ffffffff',  1,  1, 0, 0, 0, 1, state.x1, state.y0))
+    fout.write(struct.pack('ffffffff',  1, -1, 0, 0, 0, 1, state.x1, state.y1))
+    fout.write(struct.pack('ffffffff', -1, -1, 0, 0, 0, 1, state.x0, state.y1))
 
-def save(infile_path, outfile_dir):
+def save(infile_path, outfile_path):
     fin = open(infile_path, 'r')
+    fout = open(outfile_path, 'wb')
+    name = ""
+    texture = ""
+    up = State()
+    down = State()
+    hover = State()
+
     for line in fin:
-        # Get the file path
-        (output_file, trash, coords) = line.partition(':')
-        outfile_path = outfile_dir + output_file + "_button.mp3"
-        fout = open(outfile_path, 'wb')
-        
-        # Get the data
-        (button_up, trash, rest) = coords.partition(':')
-        (button_down, trash, button_hover) = rest.partition(':')
-        
-        # Write the data
-        write_coords(button_up, fout)
-        write_coords(button_down, fout)
-        write_coords(button_hover, fout)
-        fout.flush()
-        fout.close()
+        (label, trash, data) = line.partition(':')
+        if labal == 'name':
+            name = data
+        elif label == 'up_coords':
+            parse_state(data, up)
+        elif label == 'down_coords':
+            parse_state(data, down)
+        elif label == 'hover_coords':
+            parse_state(data, hover)
+        elif label == 'texture':
+            texture = data
+        elif label == 'width':
+            WIDTH = data
+        elif label == 'height':
+            HEIGHT = data
+
+    write_state(fout, up)
+    write_state(fout, down)
+    write_state(fout, hover)
     fin.close()
+    fout.flush()
+    fout.close()
     return {'FINISHED'}
 
 if __name__ == '__main__':
