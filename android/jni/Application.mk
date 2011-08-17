53ee1134a40cb5cb3d8c3cee442e54b4d65bba13
Leif Andersen <leif@leifandersen.net>
2011-08-17 13:08:34 -0600

    Revved up the android build a bit.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_STL      := gnustl_static
 APP_CPPFLAGS += -fexceptions
 APP_OPTIM    := debug
-APP_ABI := armeabi armeabi-v7a # x86 Uncomment when google get's there act together.
+APP_ABI := armeabi armeabi-v7a x86
