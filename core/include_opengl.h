3e05f8965173c2abb34abda019d640eb110fc8eb
Daniel Blakemore <DanBlakemore@gmail.com>
2011-08-23 19:58:29 -0600

    lololol compile for apple.

--- core/include_opengl.h
+++ core/include_opengl.h
@@ -19,22 +19,22 @@
     #include <GL/glu.h>
 #elif __APPLE__
     #include "TargetConditionals.h"
-    #ifdef TARGET_OS_IPHONE
-        #include <OpenGLES/ES2/gl.h>
-        #include <OpenGlES/ES2/glext.h>
-        #define __GLES__
-    #elif TARGET_IPHONE_SIMULATOR
-        #include <OpenGLES/ES2/gl.h>
-        #include <OpenGlES/ES2/glext.h>
-        #define __GLES__
-    #elif TARGET_OS_MAC
+    //#ifdef TARGET_OS_IPHONE
+    //    #include <OpenGLES/ES2/gl.h>
+    //    #include <OpenGlES/ES2/glext.h>
+    //    #define __GLES__
+    //#elif TARGET_IPHONE_SIMULATOR
+    //    #include <OpenGLES/ES2/gl.h>
+    //    #include <OpenGlES/ES2/glext.h>
+    //    #define __GLES__
+    //#elif TARGET_OS_MAC
         #ifndef GL_GLEXT_PROTOTYPES
             #define GL_GLEXT_PROTOTYPES 1
         #endif
-        #include <GL/gl.h>
-        #include <GL/glext.h>
-        #include <GL/glu.h>
-    #endif
+        #include <OpenGL/gl.h>
+        #include <OpenGL/glext.h>
+        #include <OpenGL/glu.h>
+    //#endif
 #else
     #error "OpenGL include error: Y U NO HAVE COMPATIBLE OS?!"
 #endif
